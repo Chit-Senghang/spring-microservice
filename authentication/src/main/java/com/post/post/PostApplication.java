d1712616abff1ecd65593f146c7061f3c4a3c047
Chit Senghang <senghang.chit@igsaas.asia>
2024-04-18 09:24:44 +0700

    Init docker file

--- authentication/src/main/java/com/post/post/PostApplication.java
+++ authentication/src/main/java/com/post/post/PostApplication.java
@@ -8,8 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class PostApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PostApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PostApplication.class, args);
+    }
 }
